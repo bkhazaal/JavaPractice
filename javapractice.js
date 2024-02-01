b6612091e7921967aac759c9823c1603625df977
Ben Khazaal <thajukez101@gmail.com>
2024-02-01 15:10:43 -0700

    changes

--- javapractice.js
+++ javapractice.js
@@ -2,3 +2,11 @@ function HelloWorld() {
   const myTitle = document.getElementById("schnarvus");
   myTitle.textContent = "Hello World!";
 }
+
+function PressMe() {
+  let Images = ["dog1.jpg", "dog2.jpg", "dog3.jpg"];
+  console.log("clicked");
+  let Random_Image = Math.floor(Math.random() * 3);
+  let randomImageElement = document.getElementById("randomImage");
+  randomImageElement.src = Images[Random_Image];
+}
