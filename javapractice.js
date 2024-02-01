76cf3ab305f6199441e37422f20ebaa864d65986
Ben Khazaal <thajukez101@gmail.com>
2024-02-01 16:03:30 -0700

    commit

--- javapractice.js
+++ javapractice.js
@@ -4,7 +4,7 @@ function HelloWorld() {
 }
 
 function PressMe() {
-  let Images = ["dog1.jpg", "dog2.jpg", "dog3.jpg"];
+  let Images = ["/Images/dog1.jpg", "/Images/dog2.jpg", "/Images/dog3.jpg"];
   console.log("clicked");
   let Random_Image = Math.floor(Math.random() * 3);
   let randomImageElement = document.getElementById("randomImage");
